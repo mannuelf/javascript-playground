69e5afd906d816ad50b1dd967f4edaf84d21d637
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-04-28 05:42:59 +0200

    custom map class

--- 05_TypeScript/00_maps/src/index.ts
+++ 05_TypeScript/00_maps/src/index.ts
@@ -1,10 +1,5 @@
+import { CustomMap } from './CustomMap';
 // import { User } from './user';
 // import { Company } from './company';
 
-const map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+new CustomMap('map');
