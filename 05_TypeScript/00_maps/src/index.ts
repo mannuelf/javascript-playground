dcba1cbfcc20ec580f9fec842d4b888089c76242
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-04-28 06:04:01 +0200

    duplicate code, without interfaces

--- 05_TypeScript/00_maps/src/index.ts
+++ 05_TypeScript/00_maps/src/index.ts
@@ -1,5 +1,11 @@
 import { CustomMap } from './CustomMap';
-// import { User } from './user';
-// import { Company } from './company';
+import { User } from './user';
+import { Company } from './company';
 
-new CustomMap('map');
+const user = new User();
+const company = new Company();
+
+const customMap = new CustomMap('map');
+
+customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
