88acff1fd00469c11d5f183350f3f9f04fea9bfe
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-04-29 23:25:18 +0200

    sorter

--- 05_TypeScript/00_sort/build/index.js
+++ 05_TypeScript/00_sort/build/index.js
@@ -1,6 +1,5 @@
 "use strict";
 var Sorter = /** @class */ (function () {
-    //collection: number[];
     function Sorter(collection) {
         this.collection = collection;
         this.collection = collection;
@@ -9,12 +8,19 @@ var Sorter = /** @class */ (function () {
         var length = this.collection.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                // compare lefthand side to right hand side
-                if (this.collection[j] > this.collection[j + 1]) {
-                    // get ref to left
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
+                // type gaurd check if is an array
+                if (this.collection instanceof Array) {
+                    // if numbers compare lefthand side to right hand side
+                    if (this.collection[j] > this.collection[j + 1]) {
+                        // get ref to left
+                        var leftHand = this.collection[j];
+                        this.collection[j] = this.collection[j + 1];
+                        this.collection[j + 1] = leftHand;
+                    }
+                }
+                // if collection is a string
+                if (typeof this.collection === 'string') {
+                    // string logic
                 }
             }
         }
@@ -24,5 +30,3 @@ var Sorter = /** @class */ (function () {
 var sorter = new Sorter([10, 2, 44, 100, 0]);
 sorter.sort();
 console.log(sorter.collection);
-var letterThing = '+';
-console.log(letterThing.charCodeAt());
--- 05_TypeScript/00_sort/src/index.ts
+++ 05_TypeScript/00_sort/src/index.ts
@@ -1,20 +1,26 @@
 class Sorter {
-  //collection: number[];
-
-  constructor(public collection: number[]) {
+  constructor(public collection: number[] | string) {
     this.collection = collection;
   }
 
   sort(): void {
     const { length } = this.collection;
+
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-        // compare lefthand side to right hand side
-        if (this.collection[j] > this.collection[j + 1]) {
-          // get ref to left
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        // type gaurd check if is an array
+        if (this.collection instanceof Array) {
+          // if numbers compare lefthand side to right hand side
+          if (this.collection[j] > this.collection[j + 1]) {
+            // get ref to left
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
+        }
+        // if collection is a string
+        if (typeof this.collection === 'string') {
+          // string logic
         }
       }
     }
@@ -25,5 +31,3 @@ const sorter = new Sorter([10, 2, 44, 100, 0]);
 
 sorter.sort();
 console.log(sorter.collection);
-const letterThing = '+';
-console.log(letterThing.charCodeAt());
