4ebd6404a218d0e0067c5c7fc9d1a71d2eb16861
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-04-30 06:57:26 +0200

    characters sort

--- 05_TypeScript/00_sort/build/index.js
+++ 05_TypeScript/00_sort/build/index.js
@@ -1,8 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
-var NumbersCollection_1 = require("./NumbersCollection");
-var numbersCollection = new NumbersCollection_1.NumbersCollection([100, 3, 224, 55, 1]);
-var sorter = new Sorter_1.Sorter(numbersCollection);
+var CharactersCollection_1 = require("./CharactersCollection");
+/*
+const numbersCollection = new NumbersCollection([100, 3, 224, 55, 1]);
+
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
+
 console.log(numbersCollection.data);
+*/
+var charsCollection = new CharactersCollection_1.CharactersCollection('Xoushw');
+var sorter = new Sorter_1.Sorter(charsCollection);
+sorter.sort();
+console.log(charsCollection.data);
--- 05_TypeScript/00_sort/src/index.ts
+++ 05_TypeScript/00_sort/src/index.ts
@@ -1,9 +1,17 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
+/*
 const numbersCollection = new NumbersCollection([100, 3, 224, 55, 1]);
 
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 
 console.log(numbersCollection.data);
+*/
+
+const charsCollection = new CharactersCollection('Xoushw');
+const sorter = new Sorter(charsCollection);
+sorter.sort();
+console.log(charsCollection.data);
