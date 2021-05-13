f60b48afbb6b3c0c14d133b8eac2e27330e05a6f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-13 19:01:41 +0200

    add button bound to function to fetch todos

--- 09_react_typescript/src/components/App.tsx
+++ 09_react_typescript/src/components/App.tsx
@@ -13,10 +13,17 @@ class _App extends React.Component<IAppProps> {
     this.props.fetchTodos();
   };
 
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: ITodo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  }
+
   render() {
     return (
       <div>
         <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
       </div>
     );
   }
