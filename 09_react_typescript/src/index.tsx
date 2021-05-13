968fcb183b5f3d535f1a4c156bd7787f6b41e1c5
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-13 19:09:03 +0200

    add redux devtools extension

--- 09_react_typescript/src/index.tsx
+++ 09_react_typescript/src/index.tsx
@@ -5,8 +5,12 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { App } from './components/App';
 import { reducers } from './reducers/index';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(thunk)),
+);
 
 ReactDOM.render(
   <Provider store={store}>
