6e82ab08b15ea30a6c05c2f321268cedd9dd8d8e
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-27 08:13:27 +0200

    test: guessedwords tests

--- 06_Testing-react/jotto/src/GuessedWords.js
+++ 06_Testing-react/jotto/src/GuessedWords.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GuessedWords = (props) => { return (<div />) }
+const GuessedWords = (props) => {
+  let contents;
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test="guess-instructions">Try to guess the secret word!</span>
+    )
+  }
+  return (
+    <div data-test="component-guessed-words">
+      { contents }
+    </div>
+  )
+}
 
-GuessedWords.PropTypes = {
-
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired
+    })
+  ).isRequired
 }
 
 export default GuessedWords;
--- 06_Testing-react/jotto/src/GuessedWords.test.js
+++ 06_Testing-react/jotto/src/GuessedWords.test.js
@@ -1,21 +1,51 @@
 import React from 'react';
-import { shallow } from Enzyme;
-import { findByTestAttr } from './testUtils';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps } from './testUtils';
 import GuessedWords from './GuessedWords';
 
 const defaultProps = [{
-  guessedWords: [
-    { guessedWord: 'train', letterMatchCount: 3}
-  ]
+  guessedWords: {
+    guessedWord: 'train', letterMatchCount: 3
+  }
 }];
 
 /**
  * Factory function to create a ShallowWrapper for the GuessedWords component
  * @Function setup
- * @param {objec} props -Component props specific to this setup
+ * @param {object} props -Component props specific to this setup
  * @returns {ShallowWrapper}
  */
 const setup = (props = {}) => {
   const setupProps = { ...defaultProps, ...props };
   return shallow(<GuessedWords {...setupProps} />)
- }
+}
+
+test('does not throw warning with expected props', () => {
+  checkProps(GuessedWords, defaultProps);
+});
+
+describe('if there are no words guessed', () => {
+  let wrapper;
+
+  // makes wrapper var available to all subsequent tests.
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+
+  test('renders without error', () => {
+    const component = findByTestAttr(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+
+  test('renders instructions to guess a word', () => {
+    const instructions = findByTestAttr(wrapper, 'guess-instructions');
+    expect(instructions.text().length).not.toBe(0);
+  });
+
+});
+
+describe('if there are words guessed', () => {
+  test('renders without error', () => {
+
+  })
+});
