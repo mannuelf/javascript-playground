0bade43aaf8ad25a14a4b09f3440a84da135b13a
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-28 07:25:37 +0200

    test: input test and component

---
+++ 06_Testing-react/jotto/src/Input.js
@@ -0,0 +1,13 @@
+const Input = props => {
+  return (
+    <input
+      data-test="component-input"
+      type="text"
+      name="guess"
+      id="guess"
+      className="form-control"
+    />
+  );
+};
+
+export default Input;
---
+++ 06_Testing-react/jotto/src/Input.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { findByTestAttr } from './testUtils';
+import Input from './Input';
+
+/**
+ * Setup function for app component.
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<Input />);
+};
+
+test('Input renders without error', () => {
+  const wrapper = setup();
+  const inputComponent = findByTestAttr(wrapper, 'component-input');
+  expect(inputComponent.length).toBe(1);
+});
