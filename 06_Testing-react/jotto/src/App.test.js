7280203114800ce10a59795846d5a2cc08451f3a
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-05-31 05:11:30 +0200

    test: App component test, ready for functional tests

--- 06_Testing-react/jotto/src/App.test.js
+++ 06_Testing-react/jotto/src/App.test.js
@@ -1,8 +1,18 @@
+import { shallow } from 'enzyme';
+import { findByTestAttr } from './testUtils';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+/*
+ * Setup function for App component
+ * @returns {ShallowWrapper}
+ */
+const setup = () => {
+  return shallow(<App />);
+};
+
+test('Renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent).toHaveLength(1);
 });
--- 06_Testing-react/jotto/src/helpers/index.js
+++ 06_Testing-react/jotto/src/helpers/index.js
@@ -7,6 +7,5 @@
 export function getLetterMatchCount(guessedWord, secretWord) {
   const secretLetters = secretWord.split('');
   const guessedLetterSet = new Set(guessedWord);
-  console.log(typeof guessedLetterSet, guessedLetterSet)
   return secretLetters.filter(letter => guessedLetterSet.has(letter)).length;
 }
