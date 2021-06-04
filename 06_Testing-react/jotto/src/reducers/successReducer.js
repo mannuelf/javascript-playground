1601e281822bab15fe87ed3f314666eb05a7abb5
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-04 14:40:07 +0200

    test: createad test for successReducer

--- 06_Testing-react/jotto/src/reducers/successReducer.js
+++ 06_Testing-react/jotto/src/reducers/successReducer.js
@@ -1,9 +1,18 @@
+import { actionTypes } from '../actions';
+
 /*
  * @function successReducer
  * @param {array} state - Action of guessed words.
  * @param {object} action - action to be reduced
  * @returns {boolean} - new success state
  * */
-export default (state, action) => {
-  return null;
+const sucessReducer = (state = false, action) => {
+  switch (action.type) {
+    case actionTypes.CORRECT_GUESS:
+      return true;
+    default:
+      return state;
+  }
 };
+
+export default sucessReducer;
