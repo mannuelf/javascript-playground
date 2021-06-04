1b8b687da978c092881c4e053940b3a06897d802
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-04 14:14:03 +0200

    setup: redux

--- 06_Testing-react/jotto/src/Congrats.js
+++ 06_Testing-react/jotto/src/Congrats.js
@@ -8,7 +8,7 @@ import PropTypes from 'prop-types';
  * @returns {JSX.Element} - Rendered component (or null if success) prop is false
  */
 export const Congrats = props => {
-  if (props) {
+  if (props.success) {
     return (
       <div data-test="component-congrats" className="alert alert-success">
         <span data-test="congrats-message">
