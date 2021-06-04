27189bc3a2e39cf8741824f442a3404661f0d54f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2021-06-04 14:24:34 +0200

    test: correctGuess action creator

--- 06_Testing-react/jotto/src/actions/index.js
+++ 06_Testing-react/jotto/src/actions/index.js
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+export const actionTypes = {
+  CORRECT_GUESS: 'CORRECT_GUESS',
+};
+
+/*
+ * @function corretGuess
+ * @returns {object} - Action object with type 'CORRECT_GUESS'
+ * */
+export function correctGuess() {
+  return { type: actionTypes.CORRECT_GUESS };
+}
+
 export const getSecretWord = () => {
   return axios.get('http://localhost:3030').then(response => response.data);
 };
