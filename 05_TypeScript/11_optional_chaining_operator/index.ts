274959f28b2e91f9d5c2e8f8f3b02b3948657b8e
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-03-10 15:35:47 +0100

    optional chaining and nullish coalescing

---
+++ 05_TypeScript/11_optional_chaining_operator/index.ts
@@ -0,0 +1,31 @@
+type SerializationOptions = {
+  formatting?: {
+    getIndent?: () => number;
+  };
+};
+
+function serializeJSON(value: any, options?: SerializationOptions) {
+  // GOOD: Optional Chaining Operator
+  const indent = options?.formatting?.getIndent?.() ?? 2;
+
+  // BAD
+  // const indent = options
+  //   ? options.formatting
+  //     ? options.formatting.indent
+  //     : undefined
+  //   : undefined;
+  return JSON.stringify(value, null, indent);
+}
+
+const user = {
+  name: 'Mannuel',
+  surname: 'Ferreira',
+};
+
+const json = serializeJSON(user, {
+  formatting: {
+    // getIndent: () => 8,
+  },
+});
+
+console.log(json);
