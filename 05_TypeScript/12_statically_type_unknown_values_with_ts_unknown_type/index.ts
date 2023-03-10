1938d660c3b336af6f7c5aac330cc631b93b1987
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-03-10 16:06:33 +0100

    alternate asssert function on number value, defensive.

--- 05_TypeScript/12_statically_type_unknown_values_with_ts_unknown_type/index.ts
+++ 05_TypeScript/12_statically_type_unknown_values_with_ts_unknown_type/index.ts
@@ -16,11 +16,19 @@ function assert(condition: boolean, message: string): asserts condition {
   }
 }
 
+function assertIsNumber(value: unknown, name: string): asserts value is number {
+  if (typeof value !== 'number') {
+    throw Error(`Expected value "${name}" to be a number`);
+  }
+}
+
 // function overload signature
 function range(from: number, to: number): number[];
 function range(from: unknown, to: unknown): number[] {
-  assert(typeof from === 'number', 'from must be a number');
-  assert(typeof to === 'number', 'to must be a number');
+  // assert(typeof from === 'number', 'from must be a number');
+  // assert(typeof to === 'number', 'to must be a number');
+  assertIsNumber(from, 'from');
+  assertIsNumber(to, 'to');
 
   const values: number[] = [];
   for (let i = from; i < to; i++) {
@@ -29,4 +37,5 @@ function range(from: unknown, to: unknown): number[] {
   return values;
 }
 
-console.log(range('A' as any, 'Z' as any));
+console.log(range(0, 100));
+// console.log(range('A' as any, 'Z' as any));
