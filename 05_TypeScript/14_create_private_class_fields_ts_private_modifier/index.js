be177c56f42300809cc8324e4dc1508e0bc023de
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2023-03-23 06:50:45 +0100

    user hash to make truly private variable, compiles down to Weakmap

--- 05_TypeScript/14_create_private_class_fields_ts_private_modifier/index.js
+++ 05_TypeScript/14_create_private_class_fields_ts_private_modifier/index.js
@@ -1,19 +1,31 @@
 "use strict";
 // https://egghead.io/lessons/typescript-create-private-class-fields-using-typescript-s-private-modifier
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _Counter_count;
 class Counter {
     constructor() {
-        this._value = 0;
+        _Counter_count.set(this, 0);
     }
     increment() {
-        this._value += 1;
+        __classPrivateFieldSet(this, _Counter_count, __classPrivateFieldGet(this, _Counter_count, "f") + 1, "f");
     }
     get count() {
-        return this._value;
+        return __classPrivateFieldGet(this, _Counter_count, "f");
     }
 }
+_Counter_count = new WeakMap();
 const counter = new Counter();
 counter.increment();
 counter.increment();
 counter.increment();
-counter['_value'] = 99;
 console.log(counter.count);
--- 05_TypeScript/14_create_private_class_fields_ts_private_modifier/index.ts
+++ 05_TypeScript/14_create_private_class_fields_ts_private_modifier/index.ts
@@ -1,14 +1,14 @@
 // https://egghead.io/lessons/typescript-create-private-class-fields-using-typescript-s-private-modifier
 
 class Counter {
-  private _value = 0;
+  #count = 0;
 
   increment() {
-    this._value += 1;
+    this.#count += 1;
   }
 
   get count() {
-    return this._value;
+    return this.#count;
   }
 }
 
@@ -17,5 +17,4 @@ counter.increment();
 counter.increment();
 counter.increment();
 
-counter['_value'] = 99;
 console.log(counter.count);
